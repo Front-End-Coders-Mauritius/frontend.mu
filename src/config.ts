8f3262c0b4bf3be73c88cfebb577aa966c0f036b
Sandeep Ramgolam <sandeep@ramgolam.com>
2022-09-05 19:52:49 +0400

    port more components

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Frontend Coders Mauritius';
-export const SITE_DESCRIPTION = 'Welcome';
+export const SITE_TITLE = 'Front-End Coders Mauritius';
+export const SITE_DESCRIPTION = 'Community of Front-End developers who share their passions for the web. Events, workshops and conferences occurs regularly.';
