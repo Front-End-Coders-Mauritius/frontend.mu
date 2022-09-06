23c3cafccd2842ce97ec1a2b72845da1e24649eb
Sandeep Ramgolam <sandeep@ramgolam.com>
2022-09-06 22:45:41 +0400

    update astro to work with vue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+
+import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://frontend.mu',
+  integrations: [mdx(), sitemap(), vue()],
+  vite: {
+    resolve: { dedupe: ['vue'] },
+  },
 });
---
+++ src/components/Simple.vue
@@ -0,0 +1,19 @@
+<template>
+  <div v-if="x">
+    hello
+  </div>
+  <div v-else>
+    there
+
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const x = ref(false)
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>  
