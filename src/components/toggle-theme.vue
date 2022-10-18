4eb78311b1d01b5f41c579bf063b03678b793327
Kushul Soomaree <kushul@lsl.digital>
2022-10-18 20:44:24 +0400

    vue core installation + toggle setup

---
+++ src/components/toggle-theme.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { useDark, useToggle } from '@vueuse/core'
+
+const isDark = useDark()
+const toggleDark = useToggle(isDark)
+</script>
+
+<template>
+    <button @click="toggleDark()">
+        Is Dark: {{ isDark }}
+    </button>
+</template>
