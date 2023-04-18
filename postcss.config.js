5e7875e93b9a1f851e0e29b45dbcb6e89ac3f49b
Aurazor <nikhilaukhaj590@gmail.com>
2023-04-18 10:03:39 +0400

    Removed sass + other changes.

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,8 @@ const path = require('path')
 
 module.exports = {
 	plugins: {
+		'postcss-import': {},
+    	'tailwindcss/nesting': {},
 		tailwindcss: {
 			config: path.join(__dirname, 'tailwind.config.js'), // update this if your path differs!
 		},
