d100c9dd007bbf1bdffb4b4975ac287362d97902
nicolasstrands <strands.media@gmail.com>
2023-08-01 00:17:59 +0400

    used env variables

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://pdvnhffpoacjaxobqvoq.supabase.co";
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBkdm5oZmZwb2FjamF4b2Jxdm9xIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjQ5ODY5MzIsImV4cCI6MTk4MDU2MjkzMn0.043svIx93YGKZiibIfx8SZ4--cJcVYjyE1TpAWqHMnQ";
+const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
