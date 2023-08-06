d0d752d51916a95889afd9271493408f6ea3cc01
Sandeep Ramgolam <sandeep@ramgolam.com>
2023-08-06 12:41:03 +0400

    add jetbrains mono

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,11 +9,9 @@ module.exports = {
 	theme: {
 		fontFamily: {
 			mono: ['Electrolize', 'Courier New', 'monospace'],
-			// sans: ['sans-serif'],
 			opensans: ['"Open Sans"', 'sans-serif'],
-			garamond: ['"garamond"', 'serif'],
-			fredericka: ['"fredericka"', 'serif'],
-			iosevka: ['"iosevka"', 'monospace'],
+
+			heading: ["'JetBrains Mono', monospace", 'monospace'],
 		},
 		screens: {
 			'sm': '640px',
@@ -80,7 +78,7 @@ module.exports = {
 			fontFamily: {
 				iosevka: ['Iosevka Term Bold', 'sans-serif'],
 			},
-			
+
 		},
 	},
 	plugins: [
