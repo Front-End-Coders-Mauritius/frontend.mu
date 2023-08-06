c1b0fb76db4439f32100212c5f2179111f53ea3e
Cedric <cedpoilly@gmail.com>
2023-08-06 20:23:56 +0400

    feat: mobile RSVP

--- src/pages/user/components/rsvp-radio-item.vue
+++ src/pages/user/components/rsvp-radio-item.vue
@@ -23,13 +23,14 @@ defineProps({
         <div :class="[
             active ? 'ring-2 ring-white' : '',
             checked ? 'transition-all duration-100 ring-slate-800 bg-white dark:ring-white dark:text-black ring-2  dark:hover:bg-slate-100' : 'dark:bg-slate-900 dark:ring-slate-700 ring-0 bg-white shadow-md dark:shadow-white/5 dark:ring-1 dark:ring-white/10 dark:text-white dark:hover:bg-slate-900',
-            'text-slate-700  flex items-center ring-2 rounded-md py-1 text-sm font-semibold text-center px-4 relative cursor-pointer  '
+            'text-slate-700 flex items-center ring-2 rounded-md py-1 text-sm font-semibold text-center px-4 relative cursor-pointer  ',
+            'h-12 md:h-auto'
         ]">
             <RadioGroupLabel as="div" class="select-none text-center w-full">
 
                 <Transition name="slide">
                     <IconCheckmark v-if="checked"
-                        class="bg-slate-800 dark:bg-white absolute -right-3 top-1 rounded-full w-5 h-5 text-green-600" />
+                        class="bg-slate-800 dark:bg-white absolute -right-3 top-1/2 -translate-y-1/2 rounded-full w-5 h-5 text-green-600" />
                 </Transition>
 
                 <div class="w-full text-center">
