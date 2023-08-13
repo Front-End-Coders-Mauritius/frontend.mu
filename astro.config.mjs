86ec1d3200e81d5b10ca5b1af082615d45b0aee1
houzairkoussa <houzairmk@icloud.com>
2023-08-13 22:32:37 +0400

    [dev] ADD conditional base url <- prod vs dev

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,24 @@ import Icons from 'unplugin-icons/vite'
 
 import vue from "@astrojs/vue";
 
+const SERVER_PORT = 3000;
+const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
+
+const LIVE_URL = "https://frontend.mu";
+
+// checks if app is served using astro build script
+const SCRIPT = process.env.npm_lifecycle_script || "";
+const isBuild = SCRIPT.includes("astro build");
+
+let BASE_URL = LOCALHOST_URL;
+
+if (isBuild) {
+  BASE_URL = LIVE_URL;
+}
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://frontend.mu",
+  site: BASE_URL,
   integrations: [
     mdx(),
     sitemap(),
