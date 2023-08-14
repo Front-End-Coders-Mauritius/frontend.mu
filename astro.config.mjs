806e4df555fc2828cb0758612368b43c9557b95e
houzairkoussa <houzairmk@icloud.com>
2023-08-14 18:30:00 +0400

    [fix][pr-review-79] DELETE conditonal base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,24 +6,9 @@ import Icons from 'unplugin-icons/vite'
 
 import vue from "@astrojs/vue";
 
-const SERVER_PORT = 3000;
-const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
-
-const LIVE_URL = "https://frontend.mu";
-
-// checks if app is served using astro build script
-const SCRIPT = process.env.npm_lifecycle_script || "";
-const isBuild = SCRIPT.includes("astro build");
-
-let BASE_URL = LOCALHOST_URL;
-
-if (isBuild) {
-  BASE_URL = LIVE_URL;
-}
-
 // https://astro.build/config
 export default defineConfig({
-  site: BASE_URL,
+  site: "https://frontend.mu",
   integrations: [
     mdx(),
     sitemap(),
--- src/config.ts
+++ src/config.ts
@@ -4,5 +4,3 @@
 export const SITE_TITLE = 'Front-End Coders Mauritius';
 export const SITE_DESCRIPTION = 'Community of Front-End developers who share their passions for the web. Events, workshops and conferences occurs regularly.';
 
-const BASE_URL = new URL(import.meta.env.SITE);
-export const SITE_URL = BASE_URL.origin;
