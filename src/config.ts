75b1116ef9d1076ec0a367502cebf4bf29c991f4
houzairkoussa <houzairmk@icloud.com>
2023-08-17 21:54:35 +0400

    [dev][stats-counter] ADD global site url

--- src/config.ts
+++ src/config.ts
@@ -4,3 +4,5 @@
 export const SITE_TITLE = 'Front-End Coders Mauritius';
 export const SITE_DESCRIPTION = 'Community of Front-End developers who share their passions for the web. Events, workshops and conferences occurs regularly.';
 
+const BASE_URL = new URL(import.meta.env.SITE);
+export const SITE_URL = BASE_URL.origin;
