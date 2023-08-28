53f9a1565cefa1ae872e3c89dd61839a72b2ec07
houzairkoussa <houzairmk@icloud.com>
2023-08-28 21:38:23 +0400

    [dev] ADD dump files

--- scripts/pre-build.js
+++ scripts/pre-build.js
@@ -1,9 +1,13 @@
 // any scripts occurring before astro build goes here
 const { loadSpeakers, loadEvents } = require("./directus-dump");
+const fs = require("fs");
 
 const preBuild = async () => {
   const speakers = await loadSpeakers();
   const meetups = await loadEvents();
+
+  fs.writeFileSync("src/data/speakers-raw.json" , JSON.stringify(speakers));
+  fs.writeFileSync("src/data/meetups-raw.json" , JSON.stringify(meetups));
 }
 
 preBuild();
