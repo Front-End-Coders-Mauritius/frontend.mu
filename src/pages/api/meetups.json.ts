b85c3ec65957a5cca87d5e374aeda79744eaf03f
houzairkoussa <houzairmk@icloud.com>
2023-08-31 20:43:36 +0400

    [dev] ADD stats counter

--- src/pages/api/meetups.json.ts
+++ src/pages/api/meetups.json.ts
@@ -1,48 +1,5 @@
 import type { APIRoute } from "astro";
-import eventsResponse from "@data/meetups-raw.json";
-
-const getSponsorDetails = (sponsors) => {
-  return (
-    sponsors?.map((sponsor) => ({
-      name: sponsor?.Sponsor_id?.Name || "",
-      url: sponsor?.Sponsor_id?.Website || "",
-    })) || []
-  );
-};
-
-const getSessionsDetails = (sessions) => {
-  return (
-    sessions?.map((session) => ({
-      title: session?.Session_id?.title || "",
-      speaker: session?.Session_id?.speakers?.name || "",
-      github_account: session?.Session_id?.speakers?.github_account || "",
-    })) || []
-  );
-};
-
-const meetups = (SITE_URL: string) =>
-  eventsResponse.data.reduce((acc, event) => {
-    const year = new Date(event.Date).getFullYear();
-
-    if (!acc[year]) {
-      acc[year] = [];
-    }
-
-    acc[year].push({
-      title: event?.title || "",
-      location: event?.Location || "",
-      venue: event?.Venue || "",
-      time: event?.Time || "",
-      date: event?.Date || "",
-      images: event?.images?.map((image) => image?.imagename || "") || [],
-      number_of_attendees: event?.Attendees || 0,
-      topics: getSessionsDetails(event?.sessions || []),
-      sponsors: getSponsorDetails(event?.sponsors || []),
-      live_url: `${SITE_URL}/meetup/${event?.id || ""}`,
-    });
-
-    return acc;
-  }, {});
+import { getCleanedUpMeetupsData } from "@utils/data-helpers";
 
 // response from this api looks as follows
 // grouped by year and each meantup has the following details:
@@ -60,7 +17,7 @@ export const get: APIRoute = async ({ request }) => {
   const SITE_URL = new URL(request.url).origin;
   return {
     body: JSON.stringify({
-      ...meetups(SITE_URL),
+      ...getCleanedUpMeetupsData(SITE_URL),
     }),
   };
 };
--- src/pages/api/team.json.ts
+++ src/pages/api/team.json.ts
@@ -2,16 +2,9 @@ import type { APIRoute } from "astro";
 
 import speakersResponse from "@data/speakers-raw.json";
 
-// can replace this with list of contributors from
-// https://api.github.com/repos/Front-End-Coders-Mauritius/frontendmu-astro/contributors
-import contributors from "../../data/contributors.json";
+import contributors from "@data/contributors.json";
 
-import { people } from "../../data/people";
-<<<<<<< HEAD
-=======
-
-const speakers = await loadSpeakers();
->>>>>>> main
+import { people } from "@data/people";
 
 export const get: APIRoute = async () => {
   return {
