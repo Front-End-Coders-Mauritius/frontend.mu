8c0606170b1e272050fd2dbf05af1aed18d643a4
dereck lhw <dereck.lamhonwah@gmail.com>
2023-10-15 17:41:27 +0400

    fix: add bootstrap class attribute and css

--- src/components/LoggedUser.vue
+++ src/components/LoggedUser.vue
@@ -68,7 +68,7 @@ onMounted(() => {
 
 <template>
   <a
-    class="ml-4 flex px-3 py-2 text-sm font-medium rounded-full shadow-lg bg-white/90 shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10"
+    class="ml-4 flex px-3 py-2 text-sm font-medium rounded-full shadow-lg bg-white/90 shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10 whitespace-nowrap"
     v-if="!$isUserLoggedIn"
     href="/login"
   >
