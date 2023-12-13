c061fcc781633c147ef26701ac5c181889d6052d
Sandeep Ramgolam <sandeep@ramgolam.com>
2023-12-13 11:56:21 +0400

    try to implement dierctus utils

--- src/auth-utils/useAuth.ts
+++ src/auth-utils/useAuth.ts
@@ -1,8 +1,16 @@
 import { ref, computed } from "vue";
 import type { User } from "../utils/types";
+import { createDirectus, rest, readMe } from '@directus/sdk';
+
+const DIRECTUS_PROJECT_URL = 'http://localhost:8055';
 
 let isAuth = ref(false);
+let user = ref<User | null>(null);
+let responseFromServer = ref<any>(null);
+
 export default function useAuth() {
+
+    const client = createDirectus(DIRECTUS_PROJECT_URL).with(rest());
 
     function login() {
         // login logic
@@ -18,17 +26,16 @@ export default function useAuth() {
         return isAuth.value;
     })
 
-    function register() {
-        // register logic
+    async function getCurrentUser() {
+        const result = await client.request(readMe('*.*'));
+        responseFromServer.value = result;
     }
 
     return {
         login,
         logout,
         isLoggedIn,
-        user: {
-            name: "John Doe",
-            email: "",
-        }
+        user,
+        responseFromServer
     }
 }
