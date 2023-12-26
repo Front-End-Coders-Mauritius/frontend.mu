a3131deca6dd19be37db5f426c030acdeea0c11a
Sandeep Ramgolam <sandeep@ramgolam.com>
2023-12-26 16:33:33 +0400

    fix types

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -5,7 +5,7 @@ export const random = (list: string[] | number[]) =>
 
 // a function that converts string to safe variable name
 // Path: src/utils/helpers.ts
-export const toSafeVarName = (str: string) => {
+export const toSafeVarName = (str: string | number) => {
   return str
     .toString()
     .replace(/[^a-zA-Z0-9_]/g, "_")
@@ -16,7 +16,7 @@ export const getCookieValue = (name) => (
   document.cookie.match('(^|;)\\s*' + name + '\\s*=\\s*([^;]+)')?.pop() || ''
 )
 
-export const vTransitionName = (modelType: string = "meetup", str: string) => {
+export const vTransitionName = (modelType: string = "meetup", str: string | number) => {
   return `view-transition-name: ${toSafeVarName(modelType)}-${toSafeVarName(
     str
   )}`;
