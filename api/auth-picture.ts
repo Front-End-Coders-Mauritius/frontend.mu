8d9fad49f5c034e93b962b2c3309e437edb9ef79
Sandeep Ramgolam <sandeep@ramgolam.com>
2024-01-25 18:15:34 +0400

    wip

--- api/auth-picture.ts
+++ api/auth-picture.ts
@@ -1,67 +1,25 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import axios from 'axios';
+import { echo } from './_picture';
 
 export default async (req: VercelRequest, res: VercelResponse): Promise<void> => {
 	try {
-		// Extract environment variables
-		const googleClientId = process.env.AUTH_GOOGLE_CLIENT_ID;
-		const googleClientSecret = process.env.AUTH_GOOGLE_CLIENT_SECRET;
-
 		const userId = req.headers['user-id'] as string;
 		const accessToken = req.headers['access-token'] as string;
 
-		const directusApiEndpoint = 'https://directus.frontend.mu/users/me';
-
-		let refreshToken
-
-		axios.get(directusApiEndpoint, {
-			headers: {
-				Authorization: `Bearer ${accessToken}`,
-			},
-		})
-			.then((response: any) => {
-				const userData = response.data;
-				if ('refreshToken' in userData) {
-					refreshToken = userData.refreshToken;
-				} else {
-					console.log('Refresh Token not found in the response.');
-				}
-			})
-			.catch((error: Error) => {
-				console.error('Error fetching user data:', error.message);
-			});
-
-
-		const tokenEndpoint = 'https://accounts.google.com/o/oauth2/token';
-		const tokenResponse = await axios.post(tokenEndpoint, null, {
-			params: {
-				client_id: googleClientId,
-				client_secret: googleClientSecret,
-				refresh_token: refreshToken,
-				grant_type: 'refresh_token',
-			},
-		});
-
-
-		
-		const gAccessToken = tokenResponse.data.access_token;
-
-		const userInfoEndpoint = 'https://www.googleapis.com/oauth2/v1/userinfo';
-		const userInfoResponse = await axios.get(userInfoEndpoint, {
-			params: {
-				access_token: gAccessToken,
-			},
-		});
-
-		const googleUserInfo = userInfoResponse.data;
-		var imageUrl = googleUserInfo['picture'];
-
-		console.log(`Retrieved profile picture for user ${userId} : ${imageUrl}`)
+		let shout = echo('Welcome')
 
 		res.status(200).json({
-			imageUrl: imageUrl,
-
+			userId: userId,
+			accessToken: accessToken,
+			shout: shout,
+			// imageUrl: imageUrl,
 		});
+
+		// let imageUrl = await fetchPicture(accessToken, userId)
+
+		// res.status(200).json({
+		// 	imageUrl: imageUrl,
+		// });
 	} catch (error) {
 		console.error(error);
 		res.status(500).json({ error: 'Internal Server Error' });
