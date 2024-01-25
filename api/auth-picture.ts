6bdc0ba9448376b48c32b48af2d36f20e5b39fed
Sandeep Ramgolam <sandeep@ramgolam.com>
2024-01-25 18:27:17 +0400

    get image

--- api/auth-picture.ts
+++ api/auth-picture.ts
@@ -78,20 +78,20 @@ export default async (req: VercelRequest, res: VercelResponse): Promise<void> =>
 		const userId = req.headers['user-id'] as string;
 		const accessToken = req.headers['access-token'] as string;
 
-		let shout = echo('Welcome')
-
-		res.status(200).json({
-			userId: userId,
-			accessToken: accessToken,
-			shout: shout,
-			// imageUrl: imageUrl,
-		});
-
-		// let imageUrl = await fetchPicture(accessToken, userId)
+		// let shout = await echo('Welcome')
 
 		// res.status(200).json({
-		// 	imageUrl: imageUrl,
+		// 	userId: userId,
+		// 	accessToken: accessToken,
+		// 	shout: shout,
+		// 	// imageUrl: imageUrl,
 		// });
+
+		let imageUrl = await fetchPicture(accessToken, userId)
+
+		res.status(200).json({
+			imageUrl: imageUrl,
+		});
 	} catch (error) {
 		console.error(error);
 		res.status(500).json({ error: 'Internal Server Error' });
