8a04465054e53f6d65a582e6d2939d29b460c936
Sandeep Ramgolam <sandeep@ramgolam.com>
2024-02-25 17:18:03 +0400

    add updateUserVerification function add dummy verification button

--- src/auth-utils/useAuth.ts
+++ src/auth-utils/useAuth.ts
@@ -471,6 +471,58 @@ export default function useAuth(client: DirectusClient<any> & AuthenticationClie
 
     }
 
+    async function updateUserVerification({ user_id, event_id, status }: { user_id: string, event_id: string, status: Boolean }) {
+        try {
+            isLoading.value = true;
+            const token = getCookieValue('access_token')
+
+            if (!token) {
+                isLoading.value = false;
+                throw new Error('User is not logged in')
+            }
+
+            client = await client.with(staticToken(token))
+
+            const query_object = {
+                filter: {
+                    Events_id: {
+                        _eq: event_id
+                    },
+                    directus_users_id: {
+                        _eq: user_id
+                    }
+                }
+            }
+
+            const primaryKeyQuery = await client.request(readItems('Events_directus_users', query_object));
+
+            const updates = {
+                verified: status,
+            }
+
+            const primaryKey = primaryKeyQuery[0].id
+
+            const updateMetaResult = await client.request(updateItem('Events_directus_users', primaryKey, updates));
+
+            useToast().show({
+                title: "Success!",
+                message: "User verified successfully",
+                type: "SUCCESS",
+                visible: true
+            })
+
+            // await getListOfAttendeees(event_id);
+            // await getCurrentUser();
+
+            isLoading.value = false;
+
+            return updateMetaResult
+        } catch (error) {
+            return new Error("You don't have permission for this action")
+            console.log(error)
+        }
+    }
+
     return {
         cloudFunctionUpdateProfilePicture,
         loginWithUsernameAndPassword,
@@ -490,6 +542,7 @@ export default function useAuth(client: DirectusClient<any> & AuthenticationClie
         updateUserProfile,
         cancelRsvp,
         getListOfAttendeees,
+        updateUserVerification,
         currentEventsRSVP,
         isLoading,
         avatarUrl,
--- src/components/auth/OrganiserQRCodeScanner.vue
+++ src/components/auth/OrganiserQRCodeScanner.vue
@@ -1,5 +1,11 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import { QrcodeStream } from 'vue-qrcode-reader'
+import BaseButton from '@components/base/BaseButton.vue';
+const { updateUserVerification } = useAuth(getClient());
+import useAuth, { getClient } from '../../auth-utils/useAuth';
+
+const props = defineProps<{ meetupId: string }>()
 
 type QrCodeContent = {
   rawValue: string
@@ -11,11 +17,25 @@ function onDetect(content: Array<QrCodeContent>) {
   alert(JSON.stringify(content[0].rawValue))
   // todo: post to the server
 }
+
+const dummyUser = ref({
+  user_id: "USER_ID_HERE",
+  event_id: props.meetupId,
+  status: false // set verification status to true
+})
+
+async function verifyUser() {
+  // todo @cedpoilly the parameters should come from the qr code scanned. 
+  const userDetails = await updateUserVerification(dummyUser.value)
+  // it will return the user details, so we can show those on the organizer screen to increase the level of confidence that the trasaction happened.
+  console.log(userDetails)
+}
 </script>
 
 <template>
   <div class="flex flex-col items-center justify-center">
-    <QrcodeStream @detect="onDetect" />
+    <!-- <QrcodeStream @detect="onDetect" /> -->
+    <BaseButton @click="verifyUser">Verify {{ dummyUser.user_id }}</BaseButton>
   </div>
 </template>
 
