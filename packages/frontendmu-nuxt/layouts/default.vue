32c25e30a0b56693f014b55a3ed96c7784c3bc79
Sandeep Ramgolam <sandeep@ramgolam.com>
2024-05-02 13:34:51 +0400

    add nuxt package add about page

---
+++ packages/frontendmu-nuxt/layouts/default.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        default layout
+
+        <NuxtPage />
+    </div>
+</template>
---
+++ packages/frontendmu-nuxt/pages/about.vue
@@ -0,0 +1,23 @@
+<!-- ---
+const title = "About us";
+const description = "About us";
+--- -->
+
+<template>
+    <ContentBlock>
+        <h1 class="text-center md:text-left">About us</h1>
+        <div class="prose text-verse-600 dark:text-verse-300 pt-4 pb-16 text-center md:text-left">
+            <p>
+                Front-End Coders Mauritius is a community around front-end development
+                based in Mauritius. We also organise monthly meetups free for anyone
+                interested to attend.
+            </p>
+
+            <p>
+                Do you want to know more? <a class="text-verse-600 dark:text-verse-300" href="mailto:sandeep@ramgolam.com">
+                    Get in touch with the organisers
+                </a>
+            </p>
+        </div>
+    </ContentBlock>
+</template>
