9982c9c8db72c91fb6cf623c7098574ff10669ea
Sandeep Ramgolam <sandeep@ramgolam.com>
2024-05-11 19:08:05 +0400

    configure tailwind

--- packages/frontendmu-nuxt/nuxt.config.ts
+++ packages/frontendmu-nuxt/nuxt.config.ts
@@ -1,5 +1,30 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss"],
+  css: [
+    "~/styles/global.css",
+    "~/styles/tailwind.css",
+  ],
+  // app: {
+  //   head: {
+  //     link: [
+  //       {
+  //         rel: "stylesheet",
+  //         href: "./styles/global.css",
+  //       },
+  //       {
+  //         rel: "stylesheet",
+  //         href: "./styles/tailwind.css",
+  //       }
+  //     ]
+  //   }
+  // },
+  modules: ["@nuxtjs/tailwindcss","@nuxtjs/color-mode",'nuxt-icon'
+],
+  tailwindcss: {
+    configPath: "./tailwind.config.js",
+  },
+  alias: {
+    "@astro": "../frontendmu-astro/",
+  }
 })
