31c0890b0faa22f1d9ff157a4a4ff8c714d6b8fd
Sandeep Ramgolam <sandeep@ramgolam.com>
2024-05-12 16:43:45 +0400

    add favicon

--- packages/frontendmu-nuxt/nuxt.config.ts
+++ packages/frontendmu-nuxt/nuxt.config.ts
@@ -19,5 +19,22 @@ export default defineNuxtConfig({
   alias: {
     "#astro": "../frontendmu-astro/src/",
     "#data": "../frontendmu-data/data/",
+  },
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'icon',
+          type: 'image/svg+xml',
+          href: '/favicon.svg'
+        },
+      ]
+    },
+  },
+  site: {
+    url: 'https://frontend.mu',
+    name: 'Front-End Coders Mauritius',
+    description: 'A community around frontend, tech and development in general based in Mauritius. We also organise monthly meetups free for anyone interested to attend.',
+    defaultLocale: 'en', // not needed if you have @nuxtjs/i18n installed
   }
 })
