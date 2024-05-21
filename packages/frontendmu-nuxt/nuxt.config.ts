98c5b72c0f5f65b3cd7433d1a9cf965cfdd90cad
Sandeep Ramgolam <sandeep@ramgolam.com>
2024-05-21 17:07:31 +0400

    added shadcn-vue

--- packages/frontendmu-nuxt/nuxt.config.ts
+++ packages/frontendmu-nuxt/nuxt.config.ts
@@ -53,6 +53,6 @@ export default defineNuxtConfig({
      * Directory that the component lives in.
      * @default "./components/ui"
      */
-    componentDir: './components/ui'
+    componentDir: './components/ui',
   },
 })
