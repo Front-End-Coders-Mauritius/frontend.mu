622679d572dac313ce41c2fa0148e744a782976e
Sandeep Ramgolam <sandeep@ramgolam.com>
2024-08-03 01:02:17 +0400

    fix next meetup badge ui

--- packages/frontendmu-nuxt/components/cards/EventCard.vue
+++ packages/frontendmu-nuxt/components/cards/EventCard.vue
@@ -1,6 +1,5 @@
 <template>
-  <div
-:class="[
+  <div :class="[
     event?.album ? 'col-span-2 md:col-span-1' : 'md:col-span-1 col-span-2',
     isNextMeetup ? 'border-green-600 dark:border-green-500' : 'border-verse-50 dark:border-white/10 ',
     'group group/event in-card bg-white dark:bg-verse-700/30 dark:backdrop-blur-sm border-2 rounded-xl overflow-hidden hover:border-verse-500 transition-all duration-300',
@@ -22,20 +21,27 @@
       <div class="relative z-5 flex flex-col p-4 w-full justify-between gap-4">
 
         <template v-if="event.Date">
-          <div
-:class="[
+          <div :class="[
             isUpcoming(event.Date) ? 'text-green-600 font-bold' : 'text-verse-900 dark:text-verse-300',
             'flex flex-col font-mono text-sm font-medium gap-2 w-full justify-between',
           ]">
 
 
             <!-- Title -->
-            <h3
-              class="leading-2 text-2xl font-semibold text-verse-500 dark:text-white group-hover[.in-card]:text-verse-500">
-              <div class="w-[300px] md:w-96 focus:outline-none" :title="`Meetup ${event?.title}`">
-                {{ event?.title }}
-              </div>
-            </h3>
+            <div class="flex items-center justify-between">
+              <h3
+                class="leading-2 text-2xl font-semibold text-verse-500 dark:text-white group-hover[.in-card]:text-verse-500">
+                <div class="w-[300px] md:w-96 focus:outline-none" :title="`Meetup ${event?.title}`">
+                  {{ event?.title }}
+                </div>
+              </h3>
+
+              <template v-if="isNextMeetup">
+                <span class="bg-green-700 text-sm font-mono justify-end text-white px-3 rounded-md font-bold">
+                  NEXT MEETUP
+                </span>
+              </template>
+            </div>
 
             <div class="flex items-center gap-2">
 
@@ -45,8 +51,7 @@
                 <span>{{ formatDate(new Date(event.Date), 'dd MMM yyyy') }}</span>
               </div>
 
-              <div
-v-if="event.Venue"
+              <div v-if="event.Venue"
                 class="flex gap-1 md:gap-0 items-center justify-start text-base font-medium leading-3 md:leading-5 ">
                 <Icon name="carbon:location" class="mr-1.5 h-4 w-4 flex-shrink-0 truncate " aria-hidden="true" />
                 <div class="pt-[2px] line-clamp-1 md:line-clamp-0">
@@ -57,11 +62,6 @@ v-if="event.Venue"
 
 
             </div>
-            <template v-if="isNextMeetup">
-              <span class="bg-green-700 text-sm font-mono justify-end text-white px-3 rounded-md font-bold">
-                NEXT MEETUP
-              </span>
-            </template>
           </div>
         </template>
 
@@ -69,8 +69,7 @@ v-if="event.Venue"
           <SpeakerList :event="event" />
 
           <template v-if="event.Attendees">
-            <div
-class="flex items-center border-gray-100 bg-white/70  dark:bg-verse-950/60  rounded-full px-2"
+            <div class="flex items-center border-gray-100 bg-white/70  dark:bg-verse-950/60  rounded-full px-2"
               title="Attendees">
               <Icon name="carbon:group" class="mr-1.5 h-4 w-4 flex-shrink-0 truncate " aria-hidden="true" />
               <div class="pt-[2px] line-clamp-1 md:line-clamp-0">
@@ -92,7 +91,7 @@ import photosResponse from "../../../frontendmu-data/data/photos-raw.json";
 import CardAlbum from './CardAlbum.vue';
 
 const appConfig = useAppConfig();
-const photoAlbumSource = appConfig.photoAlbumSource
+const photoAlbumSource = appConfig.photoAlbumSource as string;
 
 
 interface Props {
