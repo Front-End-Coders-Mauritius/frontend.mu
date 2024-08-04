b5b6544c4b91f65d64965395e0689eec5567fcd4
n-d-r-d-g <n.d.r.d.g@hotmail.com>
2024-08-04 23:42:34 +0400

    Added eslint rules

--- packages/frontendmu-nuxt/eslint.config.mjs
+++ packages/frontendmu-nuxt/eslint.config.mjs
@@ -16,6 +16,10 @@ export default withNuxt(
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-unused-vars': 'off',
       '@typescript-eslint/ban-ts-comment': 'off',
+      'unused-imports/no-unused-vars': 'warn',
+      'no-console': 'warn',
+      'array-callback-return': 'off',
+      'no-alert': 'off',
     },
   },
 )
