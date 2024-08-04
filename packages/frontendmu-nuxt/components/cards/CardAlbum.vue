6629bfe2e3f21a2dcdf5f6085efd854eb5177b2d
n-d-r-d-g <n.d.r.d.g@hotmail.com>
2024-08-04 17:53:37 +0400

    #164 Fixed pixelated carousel images on hero section

--- packages/frontendmu-nuxt/components/cards/CardAlbum.vue
+++ packages/frontendmu-nuxt/components/cards/CardAlbum.vue
@@ -1,7 +1,7 @@
 <template>
   <div v-if="currentAlbum" class="grid grid-cols-3 items-center h-full">
     <NuxtImg v-for="photo in currentAlbum" :key="photo" :src="`${source}/${photo}`" alt="Album Photo" width="100"
-      height="auto" class="object-cover w-full h-full block" loading="lazy" />
+      height="160" class="object-cover w-full h-full block" loading="lazy" />
   </div>
 </template>
 
