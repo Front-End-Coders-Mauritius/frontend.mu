a22fdbff54785c88ac4b4a401cdbcdf26a2fdcd0
Sandeep Ramgolam <sandeep@ramgolam.com>
2024-08-16 20:52:00 +0400

    add github auth function

--- packages/frontendmu-nuxt/components/auth/LoginForm.vue
+++ packages/frontendmu-nuxt/components/auth/LoginForm.vue
@@ -112,7 +112,7 @@ const developmentEnvironment = process.env.NODE_ENV === 'development'
               <span class="text-sm font-semibold leading-6">Google</span>
             </a>
 
-            <a href="#"
+            <a :href="oAuthLogin('github')"
               class="flex w-full items-center justify-center gap-3 rounded-md bg-[#000000]  hover:bg-black/50  px-3 py-1.5 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#24292F]"
               disabled>
               <svg class="h-5 w-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
